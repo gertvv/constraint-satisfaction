7b29ed800080bd02420784ffb71acebc8a1b3b53
Gert van Valkenhoef <gert@gertvv.nl>
2019-05-08 19:15:33 +0200

    Simple search checking intermediate solutions

--- src/index.ts
+++ src/index.ts
@@ -27,47 +27,48 @@ const variables = [
 const n = variables.length;
 const m = variables[0].values.length;
 
+const constr_eq = (ivar: number, ival: number, ioff: number, cvar: number, cval: number) => {
+  return (sol: number[][]): boolean => {
+    const i = _.findIndex(sol[ivar], (x) => x === ival);
+    if (i === undefined) return undefined;
+    const v = sol[cvar][(i + ioff) % m];
+    return v === undefined ? undefined : v === cval;
+  };
+};
+
+const constr_nq = (ivar: number, ival: number, ioff: number, cvar: number, cval: number) => {
+  return (sol: number[][]): boolean => {
+    const i = _.findIndex(sol[ivar], (x) => x === ival);
+    if (i === undefined) return undefined;
+    const v = sol[cvar][(i + ioff) % m];
+    return v === undefined ? undefined : v !== cval;
+  };
+};
+
 // !!! Puzzel klopt niet - heeft 11 oplossingen, niet 1
 const constraints = [
-  (sol: number[][]): boolean => { // 1. Het groene stuk bagage, met de naam Molstra, bevindt zich recht tegenover de reiskoffer; ...
-    const i = _.findIndex(sol[1], (x) => x === 2);
-    const j = (i + 3) % m;
-    return sol[3][i] === 2 && sol[2][j] === 2;
-  },
-  (sol: number[][]): boolean => { // 1. ... dit is niet het rode artikel D.
-    const i = 3;
-    return sol[1][i] === 4 && sol[2][i] !== 2;
-  },
-  (sol: number[][]): boolean => { // 2. Het blauwe stuk bagage ligt direct achter dat van Abby Loftus; ...
-    const i = _.findIndex(sol[1], (x) => x === 0);
-    const j = (i + 1) % m;
-    return sol[3][j] === 1;
-  },
-  (sol: number[][]): boolean => { // 2. ... dit is niet het zwarte, ...
-    const i = _.findIndex(sol[3], (x) => x === 1);
-    return sol[1][i] !== 5;
-  },
-  (sol: number[][]): boolean => { // 2. ... dat twee plaatsen voor het bruine voorbijdraait.
-    const i = _.findIndex(sol[1], (x) => x === 1);
-    const j = (i + 2) % m;
-    return sol[1][j] === 5;
-  },
-  (sol: number[][]): boolean => { // 3. De sporttas is van J. Schwartz
-    const i = _.findIndex(sol[2], (x) => x === 5);
-    return sol[3][i] === 4;
-  },
-  (sol: number[][]): boolean => { // 4. De oranje rugzak is niet van Patel
-    const i = _.findIndex(sol[2], (x) => x === 4);
-    return sol[1][i] === 3 && sol[3][i] !== 3;
-  },
-  (sol: number[][]): boolean => { // 5. Artikel C, met het label SWB, is geen reistas
-    const i = 2;
-    return sol[3][i] === 5 && sol[2][i] !== 3;
-  },
-  (sol: number[][]): boolean => { // 6. Bij E ziet u een eenvoudige handkoffer
-    const i = 4;
-    return sol[2][i] === 0;
-  },
+  // 1. Het groene stuk bagage, met de naam Molstra, bevindt zich recht tegenover de reiskoffer; ...
+  constr_eq(1, 2, 0, 3, 2),
+  constr_eq(1, 2, 3, 2, 2),
+  // 1. ... dit is niet het rode artikel D.
+  constr_eq(0, 3, 0, 1, 4),
+  constr_nq(0, 3, 0, 2, 2),
+  // 2. Het blauwe stuk bagage ligt direct achter dat van Abby Loftus; ...
+  constr_eq(1, 0, 1, 3, 1),
+  // 2. ... dit is niet het zwarte, ...
+  constr_nq(3, 1, 0, 1, 5),
+  // 2. ... dat twee plaatsen voor het bruine voorbijdraait.
+  constr_eq(1, 1, 2, 1, 5),
+  // 3. De sporttas is van J. Schwartz
+  constr_eq(2, 5, 0, 3, 4),
+  // 4. De oranje rugzak is niet van Patel
+  constr_eq(2, 4, 0, 1, 3),
+  constr_nq(2, 4, 0, 3, 3),
+  // 5. Artikel C, met het label SWB, is geen reistas
+  constr_eq(0, 2, 0, 3, 5),
+  constr_nq(0, 2, 0, 2, 3),
+  // 6. Bij E ziet u een eenvoudige handkoffer
+  constr_eq(0, 4, 0, 2, 0),
 ];
 
 const factorial = (n: number): number => {
@@ -160,13 +161,17 @@ for (let i = 0; i < 10; ++i) {
 }
 */
 
+const solutionOk = (sol: number[][]): boolean => {
+  return _.find(constraints, (c) => c(sol) === false) === undefined;
+}
+
 const t0 = Date.now();
 
 /*
 let k = 0;
 for (let i = 0; i < N; ++i) {
   const sol = decodeSolution(i);
-  const ok = _.find(constraints, (c) => !c(sol)) === undefined;
+  const ok = solutionOk(sol);
   if (ok) {
     console.log(printSolution(sol) + '\n');
   }
@@ -175,11 +180,12 @@ for (let i = 0; i < N; ++i) {
 }
 */
 
+/*
 const sol = _.range(n).map(i => _.range(m));
 let i = 0, k = 0;
 do {
   ++i;
-  const ok = _.find(constraints, (c) => !c(sol)) === undefined;
+  const ok = solutionOk(sol);
   if (ok) {
     console.log(printSolution(sol) + '\n');
   }
@@ -187,3 +193,32 @@ do {
   if (i % 100000 === 0) console.log(`${k} / ${i} = ${k/i} ${i * 1000 / (Date.now() - t0)}`);
 } while (nextSolution(sol));
 console.log(i);
+*/
+
+
+const sol = _.range(n).map(i => i === 0 ? _.range(m): _.times(m, _.constant(undefined)));
+let i = 0, k = 0;
+const search = (sol: number[][], h: number) => {
+  const clear = (sol: number[][]) => {
+    for (let j = h + 1; j < n; ++j) {
+      for (let k = 0; k < m; ++k) {
+        sol[j][k] = undefined;
+      }
+    }
+  };
+  for (let k = 0; k < m; ++k) {
+    sol[h][k] = k;
+  }
+  do {
+    clear(sol);
+    if (h === n - 1) ++i;
+    if (solutionOk(sol)) {
+      if (h === n - 1) {
+        console.log(printSolution(sol) + '\n');
+        ++k;
+      } else search(sol, h + 1);
+    }
+  } while (nextPermutation(sol[h]));
+};
+search(sol, 1);
+console.log(i, k, N, i / N, (Date.now() - t0));
